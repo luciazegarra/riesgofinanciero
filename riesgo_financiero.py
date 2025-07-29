fdc6b6695c0d8cbd5d6bb279972afdad8d56b457
luciazegarra <zegarraurionalucia@gmail.com>
2025-07-29 18:12:28 -0400

    Add files via upload

--- riesgo_financiero.py
+++ riesgo_financiero.py
@@ -5,7 +5,7 @@ Created on Mon Jul 28 17:35:10 2025
 @author: zegar
 """
 import pandas as pd
-import seaborn as sb
+import seaborn as sns
 import numpy as st 
 import streamlit as st 
 import matplotlib.pyplot as plt
@@ -17,6 +17,8 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
 
 #Mostrar o cargar los datos
 
@@ -40,6 +42,46 @@ ds = cargar_datos()
 
 st.write("Vista previa de los datos")
 st.dataframe(ds.head())
+
+#Preprocesamiento de datos o del conjunto de datos
+#Cuando la variable es ordinal categorica, se debe usar esta formula
+
+ds_encode=ds.copy() #Copia el dataset completo a otro dataset
+
+label_cols=["Historial_Credito","Nivel_Educacion"]
+le=LabelEncoder()
+for col in label_cols: 
+    ds_encode[col]=le.fit_transform(ds_encode[col])
+
+x=ds_encode.drop("Riesgo_Financiero",axis=1)
+y=ds_encode["Riesgo_Financiero"]
+y=LabelEncoder().fit_transform(y)
+
+#dividir en conjunto de entrenamiento y conjunto de test
+
+x_train, x_test, y_train, y_test=train_test_split(x,y, test_size=0.8, random_state=0)
+
+
+#Entrenar el modelo
+
+modelo=RandomForestClassifier(n_estimators=100, random_state=0)
+modelo.fit(x_train, y_train)
+score=modelo.score(x_test, y_test)
+
+st.subheader(f"Precisión del modelo: {score:.2f}")
+
+#Matriz de confusión
+
+#nos ayuda a saber que datos nos sirven y que datos no nos sirve
+
+y_pred=modelo.predict(x_test)
+mc=confusion_matrix(y_test,y_pred)
+st.subheader("Matriz de Confusión")
+fig,ax=plt.subplots()
+
+sns.heatmap(mc, annot=True, fmt="d", cmap="Blues", ax=ax)
+st.pyplot(fig)
+
 
 
 
